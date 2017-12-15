eb8744dc14d87f94e0ca46457a44ef81e160d72b
Platin <Platin@DESKTOP-NBP5SE5>
2017-12-15 12:36:24 -0200

    PlatinContext - Correções

---
+++ Platin.Domain/Entities/EntityBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Platin.Domain.Entities
+{
+    public class EntityBase
+    {
+        public int EntityId { get; set; }
+
+        public DateTime DataCadastro { get; set; }
+
+        public DateTime UltimaAtualizacao { get; set; } = DateTime.Now;
+    }
+}
