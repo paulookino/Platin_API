00f7ebcb695a65c4cb921c5460ce48fc7df97601
paulookino <paulookino@hotmail.com>
2017-12-17 20:20:02 -0200

    ProductConfiguration Atualizado

--- Platin.Infra.Data/EntityConfig/ProductConfiguration.cs
+++ Platin.Infra.Data/EntityConfig/ProductConfiguration.cs
@@ -20,6 +20,14 @@ namespace Platin.Infra.Data.EntityConfig
             Property(c => c.ShortDescription).HasColumnName("ShortDescription").HasColumnType("nvarchar");
             Property(c => c.FullDescription).HasColumnName("FullDescription").HasColumnType("nvarchar");
             Property(c => c.AdminComment).HasColumnName("AdminComment").HasColumnType("nvarchar");
+            Property(c => c.ProductTemplateId).HasColumnName("ProductTemplateId").HasColumnType("int").IsRequired();
+            Property(c => c.VendorId).HasColumnName("VendorId").HasColumnType("int").IsRequired();
+            Property(c => c.ShowOnHomePage).HasColumnName("ShowOnHomePage").HasColumnType("bit").IsRequired();
+            Property(c => c.MetaKeywords).HasColumnName("MetaKeywords").HasColumnType("nvarchar").HasMaxLength(400);
+            Property(c => c.MetaDescription).HasColumnName("MetaDescription").HasColumnType("nvarchar");
+            Property(c => c.MetaTitle).HasColumnName("MetaTitle").HasColumnType("nvarchar").HasMaxLength(400);
+            Property(c => c.AllowCustomerReviews).HasColumnName("AllowCustomerReviews").HasColumnType("bit").IsRequired();
+
 
 
 
