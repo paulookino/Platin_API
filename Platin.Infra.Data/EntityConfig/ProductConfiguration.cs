399f0c1391fd42548d6cacae9acdbb4ba4a4be3d
Platin <Platin@DESKTOP-NBP5SE5>
2017-12-18 10:06:32 -0200

    ProductConfiguration Atualização 18/12

--- Platin.Infra.Data/EntityConfig/ProductConfiguration.cs
+++ Platin.Infra.Data/EntityConfig/ProductConfiguration.cs
@@ -29,6 +29,21 @@ namespace Platin.Infra.Data.EntityConfig
             Property(c => c.AllowCustomerReviews).HasColumnName("AllowCustomerReviews").HasColumnType("bit").IsRequired();
 
 
+            Property(c => c.ApprovedRatingSum).HasColumnName("ApprovedRatingSum").HasColumnType("int").IsRequired();
+            Property(c => c.NotApprovedRatingSum).HasColumnName("NotApprovedRatingSum").HasColumnType("int").IsRequired();
+            Property(c => c.ApprovedTotalReviews).HasColumnName("ApprovedTotalReviews").HasColumnType("int").IsRequired();
+            Property(c => c.NotApprovedTotalReviews).HasColumnName("NotApprovedTotalReviews").HasColumnType("int").IsRequired();
+            Property(c => c.SubjectToAcl).HasColumnName("SubjectToAcl").HasColumnType("bit").IsRequired();
+            Property(c => c.LimitedToStores).HasColumnName("LimitedToStores").HasColumnType("bit").IsRequired();
+            Property(c => c.Sku).HasColumnName("Sku").HasColumnType("nvarchar").HasMaxLength(400);
+            Property(c => c.ManufacturerPartNumber).HasColumnName("ManufacturerPartNumber").HasColumnType("nvarchar").HasMaxLength(400);
+            Property(c => c.Gtin).HasColumnName("Gtin").HasColumnType("nvarchar").HasMaxLength(400);
+            Property(c => c.IsGiftCard).HasColumnName("IsGiftCard").HasColumnType("bit").IsRequired();
+            Property(c => c.GiftCardTypeId).HasColumnName("GiftCardTypeId").HasColumnType("int").IsRequired();
+            Property(c => c.OverriddenGiftCardAmount).HasColumnName("OverriddenGiftCardAmount").HasColumnType("decimal");
+            Property(c => c.RequireOtherProducts).HasColumnName("RequireOtherProducts").HasColumnType("bit").IsRequired();
+            Property(c => c.RequiredProductIds).HasColumnName("RequiredProductIds").HasColumnType("nvarchar").HasMaxLength(1000);
+
 
 
         }
