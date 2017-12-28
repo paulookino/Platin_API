4a8ead53d4f8c4506fe92474fd983232070f15c4
paulookino <paulookino@hotmail.com>
2017-12-28 09:42:51 -0200

    Cupom e Item Venda - Controller

---
+++ Platin_WebAPI/Controllers/CupomController.cs
@@ -0,0 +1,195 @@
+﻿using AutoMapper;
+using Platin.Application.Interfaces;
+using Platin.Domain.Entities;
+using Platin.Infra.CrossCutting.Helpers;
+using Platin_WebAPI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+using System.Web.Http.Results;
+
+namespace Platin_WebAPI.Controllers
+{
+    public class CupomController : ApiController
+    {
+        private readonly ICupomAppServiceBase _cupomAppServiceBase;
+
+        public CupomController(ICupomAppServiceBase cupomAppServiceBase)
+        {
+            _cupomAppServiceBase = cupomAppServiceBase;
+        }
+
+        [HttpPost()]
+        public HttpResponseMessage Inserir([FromBody]CupomViewModel cupomViewModel)
+        {
+            var formatter = new JsonMediaTypeFormatter();
+            string retorno = null;
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var cupomDomain = Mapper.Map<CupomViewModel, Cupom>(cupomViewModel);
+
+                    _cupomAppServiceBase.Add(cupomDomain);
+                    retorno = "Cupom " + cupomDomain.CupCodigo + "inserido com sucesso.";
+                }
+
+                var jsonSerialize = new JsonSerialize();
+                jsonSerialize.SerializarJson(formatter);
+            }
+            catch (Exception ex)
+            {
+
+                retorno = "Houve um erro interno:" + ex;
+            }
+
+            return Request.CreateResponse(System.Net.HttpStatusCode.OK, retorno, formatter);
+        }
+
+        [HttpPut()]
+        public HttpResponseMessage Editar([FromBody]CupomViewModel cupomViewModel)
+        {
+            var formatter = new JsonMediaTypeFormatter();
+            string retorno = null;
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var cupomDomain = Mapper.Map<CupomViewModel, Cupom>(cupomViewModel);
+
+                    _cupomAppServiceBase.Update(cupomDomain);
+                    retorno = "Cupom " + cupomDomain.CupCodigo + "editado com sucesso.";
+                }
+                var jsonSerialize = new JsonSerialize();
+                jsonSerialize.SerializarJson(formatter);
+            }
+            catch (Exception ex)
+            {
+
+                retorno = "Houve um erro interno:" + ex;
+            }
+
+            return Request.CreateResponse(System.Net.HttpStatusCode.OK, retorno, formatter);
+        }
+
+        [HttpPost()]
+        public HttpResponseMessage Excluir([FromBody]CupomViewModel cupomViewModel)
+        {
+            var formatter = new JsonMediaTypeFormatter();
+            string retorno = null;
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var cupomDomain = Mapper.Map<CupomViewModel, Cupom>(cupomViewModel);
+
+                    _cupomAppServiceBase.Remove(cupomDomain);
+                    retorno = "Cupom " + cupomDomain.CupCodigo + "excluído com sucesso.";
+                }
+                var jsonSerialize = new JsonSerialize();
+                jsonSerialize.SerializarJson(formatter);
+
+            }
+            catch (Exception ex)
+            {
+
+                retorno = "Houve um erro interno:" + ex;
+            }
+
+            return Request.CreateResponse(System.Net.HttpStatusCode.OK, retorno, formatter);
+        }
+
+        [HttpGet]
+        public HttpResponseMessage Listar()
+        {
+            var formatter = new JsonMediaTypeFormatter();
+            List<Cupom> retorno = null;
+
+            string erro = null;
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    retorno = _cupomAppServiceBase.GetAll().ToList();
+                }
+                var jsonSerialize = new JsonSerialize();
+                jsonSerialize.SerializarJson(formatter);
+            }
+            catch (Exception ex)
+            {
+
+                erro = "Houve um erro interno:" + ex;
+            }
+
+            return Request.CreateResponse(System.Net.HttpStatusCode.OK, new { retorno, erro }, formatter);
+        }
+
+        [HttpGet]
+        public HttpResponseMessage BuscarPorId(int id)
+        {
+            Cupom retorno = null;
+            var formatter = new JsonMediaTypeFormatter();
+            string mensagem = null;
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    retorno = _cupomAppServiceBase.GetById(id);
+
+                    if (retorno == null)
+                    {
+                        mensagem = "Cupom por Id não encontrado.";
+                    }
+                }
+                var jsonSerialize = new JsonSerialize();
+                jsonSerialize.SerializarJson(formatter);
+            }
+            catch (Exception ex)
+            {
+
+                mensagem = "Houve um erro interno:" + ex;
+            }
+
+            return Request.CreateResponse(System.Net.HttpStatusCode.OK, new { retorno, mensagem }, formatter);
+        }
+
+        [HttpGet]
+        public HttpResponseMessage BuscarPorCodigo(string codigo)
+        {
+            List<Cupom> retorno = null;
+            var formatter = new JsonMediaTypeFormatter();
+            string mensagem = null;
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    retorno = _cupomAppServiceBase.GetByFilter(p => p.CupCodigo == codigo).ToList();
+
+                    if (retorno.Count == 0)
+                    {
+                        mensagem = "Cupom não encontrado.";
+                    }
+                }
+                var jsonSerialize = new JsonSerialize();
+                jsonSerialize.SerializarJson(formatter);
+            }
+            catch (Exception ex)
+            {
+
+                mensagem = "Houve um erro interno:" + ex;
+            }
+
+            return Request.CreateResponse(System.Net.HttpStatusCode.OK, new { retorno, mensagem }, formatter);
+        }
+
+
+        [HttpGet]
+        public IEnumerable<Cupom> RetornaTodosCodigos()
+        {
+            return _cupomAppServiceBase.GetAll();
+        }
+    }
+}
---
+++ Platin_WebAPI/Controllers/ItemVendaController.cs
@@ -0,0 +1,195 @@
+﻿using AutoMapper;
+using Platin.Application.Interfaces;
+using Platin.Domain.Entities;
+using Platin.Infra.CrossCutting.Helpers;
+using Platin_WebAPI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+using System.Web.Http.Results;
+
+namespace Platin_WebAPI.Controllers
+{
+    public class ItemVendaController : ApiController
+    {
+        private readonly IItemVendaAppServiceBase _itemVendaAppServiceBase;
+
+        public ItemVendaController(IItemVendaAppServiceBase itemVendaAppServiceBase)
+        {
+            _itemVendaAppServiceBase = itemVendaAppServiceBase;
+        }
+
+        [HttpPost()]
+        public HttpResponseMessage Inserir([FromBody]ItemVendaViewModel itemVendaViewModel)
+        {
+            var formatter = new JsonMediaTypeFormatter();
+            string retorno = null;
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var itemVendaDomain = Mapper.Map<ItemVendaViewModel, ItemVenda>(itemVendaViewModel);
+
+                    _itemVendaAppServiceBase.Add(itemVendaDomain);
+                    retorno = "Item da Venda " + itemVendaDomain.IteId + "inserido com sucesso.";
+                }
+
+                var jsonSerialize = new JsonSerialize();
+                jsonSerialize.SerializarJson(formatter);
+            }
+            catch (Exception ex)
+            {
+
+                retorno = "Houve um erro interno:" + ex;
+            }
+
+            return Request.CreateResponse(System.Net.HttpStatusCode.OK, retorno, formatter);
+        }
+
+        [HttpPut()]
+        public HttpResponseMessage Editar([FromBody]ItemVendaViewModel itemVendaViewModel)
+        {
+            var formatter = new JsonMediaTypeFormatter();
+            string retorno = null;
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var itemVendaDomain = Mapper.Map<ItemVendaViewModel, ItemVenda>(itemVendaViewModel);
+
+                    _itemVendaAppServiceBase.Update(itemVendaDomain);
+                    retorno = "Item da Venda " + itemVendaDomain.IteId + "editado com sucesso.";
+                }
+                var jsonSerialize = new JsonSerialize();
+                jsonSerialize.SerializarJson(formatter);
+            }
+            catch (Exception ex)
+            {
+
+                retorno = "Houve um erro interno:" + ex;
+            }
+
+            return Request.CreateResponse(System.Net.HttpStatusCode.OK, retorno, formatter);
+        }
+
+        [HttpPost()]
+        public HttpResponseMessage Excluir([FromBody]ItemVendaViewModel itemVendaViewModel)
+        {
+            var formatter = new JsonMediaTypeFormatter();
+            string retorno = null;
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var itemVendaDomain = Mapper.Map<ItemVendaViewModel, ItemVenda>(itemVendaViewModel);
+
+                    _itemVendaAppServiceBase.Remove(itemVendaDomain);
+                    retorno = "Item da Venda " + itemVendaDomain.IteId + "excluído com sucesso.";
+                }
+                var jsonSerialize = new JsonSerialize();
+                jsonSerialize.SerializarJson(formatter);
+
+            }
+            catch (Exception ex)
+            {
+
+                retorno = "Houve um erro interno:" + ex;
+            }
+
+            return Request.CreateResponse(System.Net.HttpStatusCode.OK, retorno, formatter);
+        }
+
+        [HttpGet]
+        public HttpResponseMessage Listar()
+        {
+            var formatter = new JsonMediaTypeFormatter();
+            List<ItemVenda> retorno = null;
+
+            string erro = null;
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    retorno = _itemVendaAppServiceBase.GetAll().ToList();
+                }
+                var jsonSerialize = new JsonSerialize();
+                jsonSerialize.SerializarJson(formatter);
+            }
+            catch (Exception ex)
+            {
+
+                erro = "Houve um erro interno:" + ex;
+            }
+
+            return Request.CreateResponse(System.Net.HttpStatusCode.OK, new { retorno, erro }, formatter);
+        }
+
+        [HttpGet]
+        public HttpResponseMessage BuscarPorId(int id)
+        {
+            ItemVenda retorno = null;
+            var formatter = new JsonMediaTypeFormatter();
+            string mensagem = null;
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    retorno = _itemVendaAppServiceBase.GetById(id);
+
+                    if (retorno == null)
+                    {
+                        mensagem = "Item da Venda por Id não encontrado.";
+                    }
+                }
+                var jsonSerialize = new JsonSerialize();
+                jsonSerialize.SerializarJson(formatter);
+            }
+            catch (Exception ex)
+            {
+
+                mensagem = "Houve um erro interno:" + ex;
+            }
+
+            return Request.CreateResponse(System.Net.HttpStatusCode.OK, new { retorno, mensagem }, formatter);
+        }
+
+        [HttpGet]
+        public HttpResponseMessage BuscarPorInformacao(string informacao)
+        {
+            List<ItemVenda> retorno = null;
+            var formatter = new JsonMediaTypeFormatter();
+            string mensagem = null;
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    retorno = _itemVendaAppServiceBase.GetByFilter(p => p.IteInformacao == informacao).ToList();
+
+                    if (retorno.Count == 0)
+                    {
+                        mensagem = "Item de Venda não encontrado.";
+                    }
+                }
+                var jsonSerialize = new JsonSerialize();
+                jsonSerialize.SerializarJson(formatter);
+            }
+            catch (Exception ex)
+            {
+
+                mensagem = "Houve um erro interno:" + ex;
+            }
+
+            return Request.CreateResponse(System.Net.HttpStatusCode.OK, new { retorno, mensagem }, formatter);
+        }
+
+
+        [HttpGet]
+        public IEnumerable<ItemVenda> RetornaTodosItemVenda()
+        {
+            return _itemVendaAppServiceBase.GetAll();
+        }
+    }
+}
